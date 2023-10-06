8658d47af2065d0da6881789a44346624989b9b7
Miguel <ohyeahs4@hotmail.com>
2023-10-06 14:38:03 -0500

    run.sh updated

--- run.sh
+++ run.sh
@@ -1,11 +1,8 @@
-#compiling lex
-flex LAB01.l
-
-# from lex to c
+# compiling from lex to c
 flex -o LAB01.yy.cpp LAB01.l
 
 # from c to out
-g++ -std=c++11 -o LAB01.out LAB01.yy.cpp -lfl
+g++ -o LAB01.out LAB01.yy.cpp
 
-#running the .exe
+#running the executable
 ./LAB01.out Entrada.txt > Salida.txt
